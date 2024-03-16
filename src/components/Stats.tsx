5ce62688fe17da65fb5b921408c66bde2f3e3994
eppu-peltonen <22355166+eppu-peltonen@users.noreply.github.com>
2024-03-16 14:36:53 +0200

    Remove padding from minutes display

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -13,7 +13,7 @@ const Stats = ({time}: StatsProps) => {
         <div className="m-auto bg-slate-700 rounded-xl p-2 flex flex-col items-center min-h-96 min-w-80">
             <span className="text-white font-bold text-3xl mt-2">Voitit pelin!</span>
             <span className="bg-slate-500 rounded-xl shadow-lg text-white font-bold p-2 mt-10">
-                Aika: {hours}h {minutes.toString().padStart(2, "0")}min {seconds.toString().padStart(2, "0")}s {milliseconds.toString().padStart(2, "0")}mm
+                Aika: {hours}h {minutes}min {seconds.toString().padStart(2, "0")}s {milliseconds.toString().padStart(2, "0")}mm
             </span>
         </div>
     );
